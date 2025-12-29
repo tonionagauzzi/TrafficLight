ccdd55066801b7c6546d56be4b6572e453cb3c12
tonionagauzzi <vitantonionagauzzi@gmail.com>
2025-12-29 23:44:37 +0900

    SDKバージョンを35から36に更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.vitantonio.nagauzzi.trafficlight"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.vitantonio.nagauzzi.trafficlight"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
